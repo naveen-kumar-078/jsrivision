d5f17ba61671927d52c9bb9a20d28bc4fb1f2a95
naveen-kumar-078 <naveenkumar071224@gmail.com>
2025-09-08 11:07:27 +0530

    my first commit

---
+++ js.js
@@ -0,0 +1,36 @@
+/*let grade  = "B";
+
+switch (grade) {
+    case "s":
+        alert("super")
+     break;
+
+    case "A":
+        alert("wow");
+        break;
+
+    default:
+        alert("very very wrong")
+}*/
+
+
+
+
+let mark = 55
+
+switch (true) {
+    case mark >90:
+        alert("pass")
+        break;
+
+    case mark >50:
+        alert("excellent")
+        break;
+
+    case mark <50:
+        alert("fail")
+        break;  
+
+    default:
+        alert("unknown grade")
+}
